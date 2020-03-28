4c138f7b5affb8fbca1fb99c807acbdad31102d9
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-03-27 22:00:31 -0700

    finished backend

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Successfully Running..."));
 
 app.use("/api/users", require("./routes/api/users"));
